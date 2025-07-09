770c54420fcdec0b90cd51a4f7459f9fff5173fd
JaeYoung <kimjaeyoung650@gmail.com>
2025-07-09 16:00:55 +0900

    feat: 연동 로직 및 연결 확인용 코드 추가

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import Redis from 'ioredis';
 
 @Injectable()
-export class RedisService {}
+export class RedisService implements OnModuleInit, OnModuleDestroy{
+  private redis: Redis;
+
+  async onModuleInit() {
+    this.redis = new Redis({
+      host: 'localhost',
+      port: 6379,
+      password: '',
+      db: 0,
+    });
+
+    this.redis.on('connect', () => {
+      console.log('Redis connected successfully');
+    });
+
+    this.redis.on('error', (err) => {
+      console.error('Redis Client Error:', err);
+    });
+
+    this.redis.on('ready', () => {
+      console.log('Redis client is ready');
+    });
+
+    this.redis.on('close', () => {
+      console.log('Redis connection closed');
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.redis.quit();
+  }
+  // Redis 연결 상태 확인
+  async ping(): Promise<string> {
+    return await this.redis.ping();
+  }
+  // Redis 서버 정보 조회
+  async info(): Promise<string> {
+    return await this.redis.info();
+  }
+}
