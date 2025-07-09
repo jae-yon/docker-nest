3d0c22efbe6a5d8f0cd69553b86e8a80ff2fdae6
JaeYoung <kimjaeyoung650@gmail.com>
2025-07-09 15:48:19 +0900

    feat: 콘솔 출력 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
 
   const config = app.get(ConfigService);
@@ -11,5 +14,11 @@ async function bootstrap() {
   const port = config.get<number>('PORT');
 
   await app.listen(port ?? 3000);
+
+  logger.log(`Application is running on: http://localhost:${port}`);
 }
-bootstrap();
+
+bootstrap().catch((error) => {
+  const logger = new Logger('Bootstrap');
+  logger.error('Failed to start application', error);
+});
